f981ea938b71622bf9c3953edf6724a7a2f57605
its-MAHADi <mhkhandakar57@gmail.com>
2025-03-07 00:24:26 +0600

    class practice ES6

---
+++ class.js
@@ -0,0 +1,20 @@
+class product{
+    constructor(name,price,brand,malik){
+        this.name = name;
+        this.price = price;
+        this.brand = brand;
+        this.malik = malik;
+    }
+
+    details() {
+        // console.log("ami datails", this.name,this.brand,this.price,this.malik) 
+        console.log(`ami ${this.name} PRODUCT er details`)
+    }
+}
+
+const iphone = new product("iphone","1234","apple","jobs");
+iphone.details()
+
+const xiaomi = new product("redmi",1000, "xiaomi","jani na");
+console.log(xiaomi)
+xiaomi.details()
